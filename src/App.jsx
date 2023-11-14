b2a0869e8257204f4b445e0d392b01d86cf6b558
GarrixA <aphrogarrix10@gmail.com>
2023-11-14 22:02:33 +0200

    struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
+import LeftSide from './Components/LeftSide';
+import TopSide from './Components/TopSide';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route index element={<Home/>}/>
+            <Route path='left' element={<LeftSide/>}/>
+            <Route path='top' element={<TopSide/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Components/TopSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopSide() {
+    return (
+        <div>
+            Top
+        </div>
+    )
+}
+
+export default TopSide
